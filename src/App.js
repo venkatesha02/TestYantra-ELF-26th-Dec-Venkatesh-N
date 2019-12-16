a107a54e45f00854501560025a75fe0d336621ef
venkatesh <venkatesh000venki@gmail.com>
2019-12-16 19:37:12 +0530

    committ

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import RecipeList from './component/recipeList/RecipeList'
+import RecipeDetails from './component/recipeDetails/RecipeDetails'
+import Headre from './Header'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    show: false,
+    id: null,
+    recipes: [
+      {
+        id: 1,
+        name: 'Jamun',
+        image: 'https://5.imimg.com/data5/IX/BL/GLADMIN-48173418/dry-jamoon-500x500.png',
+        ingridiants: ['MTR mix, ', 'Sugur, ']
+      },
+      {
+        id: 2,
+        name: 'Gobi',
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMXw5cPhDp9Q-hXCOswCofrTosAvqOS7nhsectoVKyjjHzKNpc&s',
+        ingridiants: ['Onian, ', 'Cornflower, ']
+      },
+      {
+        id: 3,
+        name: 'Pani Poori',
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7THLiDvCIiGsMUQcGlRI9Pc6F43-1oMOiI7aO7OXWZTBbO2lDhQ&s',
+        ingridiants: ['Onian, ', 'Tomoto, ']
+      },
+      {
+        id: 4,
+        name: 'Noodels',
+        image: 'https://www.recipetineats.com/wp-content/uploads/2019/11/Lo-Mein_5.jpg',
+        ingridiants: ['1 Noodels, ', '1 Cup Water, ', 'Salt, ']
+      }
+    ]
+  }
+
+  render() {
+    return (
+      <div>
+        <Headre />
+        <div className='row'>
+          <div className='col-md-4'>
+            <RecipeList list={this.state.recipes} action={this.click} />
+          </div>
+          <div className='col-md-4'>
+            {this.state.show ? <RecipeDetails details={this.state.id} /> : ""}
+          </div>
+        </div>
+      </div>)
+  }
+
+  click = (click) => {
+    console.log(click)
+    this.setState({
+      show: true,
+      id: click
+
+    })
+  }
 }
 
 export default App;
